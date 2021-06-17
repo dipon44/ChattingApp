b5c7d4e19f15b2f6090e56beceba7d496c88478a
dipon44 <dipon232@gmail.com>
2021-06-17 08:47:15 +0600

    First Commit

---
+++ API/DattingAPP/DattingAPP/Data/DattingAppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using DattingAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace DattingAPP.Data
+{
+    public class DattingAppDBContext : DbContext
+    {
+        public DattingAppDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
