33b16d66713a93f7130e226b6adfd9228e691d5c
dipon44 <dipon232@gmail.com>
2021-06-18 15:23:14 +0600

    1.APIInitialize_2.DateBaseCreate 3.MakecodeAsyncronys

---
+++ API/DattingAPP/DattingAPP/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DattingAPP.Data;
+using DattingAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingAPP.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]/[Action]")]
+    public class UsersController : ControllerBase
+    {
+        public DattingAppDBContext _dattingAppDBContext;
+        public UsersController(DattingAppDBContext dattingAppDBContext)
+        {
+            _dattingAppDBContext = dattingAppDBContext;
+        }
+
+        //get user List
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>>  GetUsers()
+        {
+            var users = await _dattingAppDBContext.Users.ToListAsync();
+            return  users;
+        }
+
+
+        //getuserbyId
+        [HttpGet]
+        public async Task<ActionResult<AppUser>> GetUserById(int Id)
+        {
+            var user = await _dattingAppDBContext.Users.FindAsync(Id);
+            return user;
+        }
+    }
+}
--- API/DattingAPP/DattingAPP/Startup.cs
+++ API/DattingAPP/DattingAPP/Startup.cs
@@ -17,22 +17,24 @@ namespace DattingAPP
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration _config { get; }
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
+         
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
 
-            services.AddDbContext<DattingAppDBContext>(options =>
+            services.AddDbContext<Data.DattingAppDBContext>(options =>
 
             {
-                options.UseSqlServer("Connection String");
+                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
         }
