5fea42ff9bef2dbb57747998f65690eeed0f6a06
dipon44 <dipon232@gmail.com>
2021-06-19 13:22:51 +0600

    HttpRequestAdd

--- ClientSide/ChattingAppClient/src/app/app.component.ts
+++ ClientSide/ChattingAppClient/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// import { Console } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ChattingAppClient';
+  users:any;
+  constructor(private http:HttpClient){}
+  ngOnInit()
+  {
+       this.getUSers();
+  }
+
+  getUSers()
+  {
+    this.http.get('htpps://localhost:5001/api/Users').subscribe(response =>
+      {
+        this.users=response;
+      },error =>
+      {
+        console.log(error);
+      })
+  }
 }
+
